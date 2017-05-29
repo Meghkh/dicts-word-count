50e3390bc0ebf169ddde69953d52f0b1b82f2f2e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-29 13:02:57 -0700

    Cleaning up and making adjustments

--- wordcount.py
+++ wordcount.py
@@ -6,15 +6,16 @@ def count_words(data_file):
     """
     word_count = {}
     file_name = open(data_file)
+
     for line in file_name:
         line = line.rstrip().split(' ')
-#        line = line.split(' ')
         for word in line:
-            if word in word_count:
-                word_count[word] += 1
+            if word == "I":
+                word_count[word] = word_count.get(word, 0) + 1
             else:
-                word_count[word] = 1
+                word_count[word.lower()] = word_count.get(word, 0) + 1
+
     for key, value in word_count.iteritems():
         print key, value
 
-count_words("twain.txt")
+count_words("test.txt")
